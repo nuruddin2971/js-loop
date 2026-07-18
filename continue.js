4659324d4130cd0b7d3814c93db383da0a1e5edc
nuruddin2971 <nuruddin2971@gmail.com>
2026-07-18 18:21:04 +0600

    loop continue system

---
+++ continue.js
@@ -0,0 +1,13 @@
+// for (let i = 0; i <= 10; i++) {
+//   if (i === 2) {
+//     continue;
+//   }
+//   console.log("value of i", i);
+// }
+
+for (let i = 0; i <= 10; i++) {
+  if (i % 2 === 0) {
+    continue;
+  }
+  console.log("value of i", i);
+}
