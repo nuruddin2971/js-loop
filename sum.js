df9aec27398b2227b81c10c7e3394fc2742962a2
nuruddin2971 <nuruddin2971@gmail.com>
2026-07-18 16:45:10 +0600

    how to sum odd and even number

---
+++ sum.js
@@ -0,0 +1,11 @@
+// let sum = 0;
+// for (let i = 0; i <= 10; i++) {
+//   sum = sum + i;
+//   console.log(i, sum);
+// }
+
+let sum = 0;
+for (let i = 2; i <= 20; i = i + 2) {
+  sum = sum + i;
+  console.log(i, sum);
+}
