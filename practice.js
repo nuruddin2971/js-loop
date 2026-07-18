7ed0e58ee0435c2fa41aa7f6f5534e24dcf91bff
nuruddin2971 <nuruddin2971@gmail.com>
2026-07-18 19:56:43 +0600

    complete task-6

---
+++ practice.js
@@ -0,0 +1,43 @@
+// Task-1
+// for (let i = 1; i <= 5; i++) {
+//   console.log("Hello");
+// }
+
+// Task-2
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// Task-3
+// let count = 10;
+// while (count >= 1) {
+//   console.log(count);
+//   count--;
+// }
+
+// Task-4
+// let count = 1;
+// while (count <= 7) {
+//   console.log("Loop continue");
+//   count++;
+// }
+
+// Task-5
+// for (let i = 1; i <= 30; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// Task-6
+// let sum = 0;
+// for (let i = 1; i <= 20; i++) {
+//   sum = sum + i;
+//   console.log(i, sum);
+// }
+
+// Task-7
+const number = 7
+for(let i = 1; i <=10; i++){
+
+}
