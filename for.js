38f71e39c3de4e8e854ba7fb8ac634e0605907a7
nuruddin2971 <nuruddin2971@gmail.com>
2026-07-18 14:20:42 +0600

    first commit

---
+++ for.js
@@ -0,0 +1,11 @@
+/**
+ * for(initialization; condition; modification){
+ * repetitive
+ * }
+ *
+ *
+ */
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
---
+++ loop.js
@@ -0,0 +1,10 @@
+// Structure of for loop:
+
+// for (initialization ; condition ; modification){
+//     repetitive tasks
+// }
+
+for (let count = 0; count < 5; count++) {
+    console.log("do something")
+}
+
